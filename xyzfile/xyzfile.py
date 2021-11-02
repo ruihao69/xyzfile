61e0dcb576c19084cfafc1523b4cf5a4aaaf8c4f
Ruihao Bi <brh6@qq.com>
2021-11-02 11:25:29 +0800

    adds 'element'; returns element list of this xyzfile

--- xyzfile/xyzfile.py
+++ xyzfile/xyzfile.py
@@ -7,7 +7,13 @@ class XYZFile(object):
     def __init__(self, filename):
         self.fhandle = open(filename, 'r')
         self.length = int(self.fhandle.readline())
-        self.data = np.empty((self.length*3), dtype=float)
+
+        # element info
+        self.elements = ['placeholder']*self.length
+        self.get_elements(filename)
+
+        # xyz data into flatten numpy array
+        self.xyz = np.empty((self.length*3), dtype=float)
         self.load_next_frame(is_first = True)
 
     def __iter__(self):
@@ -15,7 +21,16 @@ class XYZFile(object):
 
     def __next__(self):
         self.load_next_frame()
-        return self.data
+        return self.xyz
+
+    def get_elements(self, filename):
+        """
+        obtains element info from only the first frame
+        """
+        with open(filename) as xyzfile:
+            head = [next(xyzfile) for x in range(self.length+2)][2:]
+            for ii in range(self.length):
+                self.elements[ii] = re.split(' +', head[ii].strip())[0]
 
     def load_next_frame(self, is_first = False):
         exists = self.fhandle.readline()
@@ -26,4 +41,4 @@ class XYZFile(object):
 
         for i in range(self.length):
             parts = re.split(' +', self.fhandle.readline().strip())
-            self.data[i*3:i*3+3] = parts[1:]
+            self.xyz[i*3:i*3+3] = parts[1:]
